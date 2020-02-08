b0817df894ca49be897de6610244c396e6d11e80
cvelenn <cvelenn@gmail.com>
2020-02-08 17:02:24 +0100

    mail

--- mail.js
+++ mail.js
@@ -1,24 +1,35 @@
 var nodemailer = require("nodemailer");
 
-var transporter = nodemailer.createTransport({
-  service: "yahoo",
-  auth: {
-    user: "cvelenn@yahoo.com",
-    pass: "130486yahoo"
-  }
-});
+async function main() {
 
-var mailOptions = {
-    from: "youremail@gmail.com",
-    to: "cvelenn@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!"
-  };
+    let testAccount = await nodemailer.createTestAccount();
 
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass // generated ethereal password
+        }
+    });
+
+    var mailOptions = {
+        from: "youremail@gmail.com",
+        to: "cvelenn@gmail.com",
+        subject: "Sending Email using Node.js",
+        text: "That was easy!"
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+        console.log(error);
+        } else {
+        console.log("Email sent: " + info.response);
+        }
+    });
+
+}
+
+main().catch(console.error);
