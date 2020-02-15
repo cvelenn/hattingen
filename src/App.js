6b795103935b6bdd51b8f02d7c53bd492ddb34af
Cvetko Zubanov <cvetko.zubanov@servalit.com>
2020-02-15 10:05:18 +0100

    init image

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 import React from 'react';
 import Home from './Home';
 import About from './About';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <Router>
       <div>
-      <Helmet>
-        <title>Here's the Title!</title>
-        <meta name="description" content="This is what you want to show as the page content in the Google SERP Listing" />
-      </Helmet>
-      <Navbar bg="light" expand="lg"> 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavItem className="nav-link" href="/" to="/"><Link to="/">Home</Link></NavItem>
-            <NavItem className="nav-link" href="/about" to="/"> <Link to="/about">About</Link></NavItem>
-          </Nav>
+        <Helmet>
+          <title>Here's the Title!</title>
+          <meta
+            name='description'
+            content='This is what you want to show as the page content in the Google SERP Listing'
+          />
+        </Helmet>
+        <Navbar bg='light' expand='lg'>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto'>
+              <NavItem className='nav-link' href='/' to='/'>
+                <Link to='/'>Home</Link>
+              </NavItem>
+              <NavItem className='nav-link' href='/about' to='/'>
+                {' '}
+                <Link to='/about'>About</Link>
+              </NavItem>
+            </Nav>
           </Navbar.Collapse>
-      </Navbar>
+        </Navbar>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
+          <Route path='/about'>
             <About />
           </Route>
-          <Route path="/">
+          <Route path='/'>
             <Home />
           </Route>
         </Switch>
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet';
 
 function Home() {
-    return <h2>
-        <Helmet>
-            <title>Home</title>
-            <meta name="description" content="This is what you want to show as the page content in the Google SERP Listing" />
-        </Helmet>
-        Home
-    </h2>;
+  return (
+    <div>
+      <Helmet>
+        <title>Home</title>
+        <meta
+          name='description'
+          content='This is what you want to show as the page content in the Google SERP Listing'
+        />
+      </Helmet>
+      <img class='fit' alt='' src='public\images\shutterstock_563427154.jpg' />
+    </div>
+  );
 }
 
 export default Home;
